7f966492f80e36e2efcfdd776328ab79efad269c
Omar Shaikh <oshaikh13@gmail.com>
2015-12-26 01:32:05 +0530

    Add a define module

--- SimpleBot/bot.js
+++ SimpleBot/bot.js
@@ -8,7 +8,12 @@ login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD}, function
     if(err) return console.error(err);
 
     var loader = require("../botModules/loader/loader.js")(api);
-    loader.init(['quiz']);
+    loader.init(
+      [
+        {name: 'quiz', args: {}}, 
+        {name: 'define', args: {WORDNIK_API_KEY: process.env.WORDNIK_API_KEY}}
+      ]
+    );
  
     api.listen(function callback(err, message) {
 
@@ -17,13 +22,32 @@ login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD}, function
       // Just trolls. Make a troll module
       if (message.body.toLowerCase().indexOf("musab") > -1) {
         api.sendMessage("Speaking of Musab, I think he's an idiot.", message.threadID);
-      } else if (message.body.toLowerCase().indexOf("history") > -1) {
+      } 
+
+      if (message.body.toLowerCase().indexOf("history") > -1) {
         api.sendMessage("LOL TC AP history", message.threadID);
-      } else if (message.body.toLowerCase().indexOf("leonard") > -1 || 
+      } 
+
+      if (message.body.toLowerCase().indexOf("leonard") > -1 || 
           message.body.toLowerCase().indexOf("abdallah") > -1) {
         api.sendMessage("You mean the snake...", message.threadID);
-      } else if (message.body.toLowerCase().indexOf("jawad") > -1) {
+      } 
+
+      if (message.body.toLowerCase().indexOf("jawad") > -1) {
         api.sendMessage("Jawad hearthstoner af...", message.threadID);
+      }
+
+      if (message.body.toLowerCase().indexOf("nofel") > -1) {
+        api.sendMessage("All hail the nofy bear, the nofy pear, the nofy hair, the nofy insurance care",
+         message.threadID);
+      }
+
+      if (message.senderName.toLowerCase().indexOf("yaser") > -1) {
+        var factor = Math.random();
+        console.log(factor);
+        if (factor <= .33) {
+          api.sendMessage("Silence. Yaser has spoken. ", message.threadID);
+        }
       }
 
 
--- botModules/loader/loader.js
+++ botModules/loader/loader.js
@@ -8,13 +8,15 @@ module.exports = function (api) {
 
     init: function (moduleArrayList) {
       moduleArrayList.forEach(function(module){
-        this.modules[module] = require('../' + module + '/' + module + '.js');
+        this.modules[module.name] = {};
+        this.modules[module.name].create = require('../' + module.name + '/' + module.name + '.js');
+        this.modules[module.name].args = module.args;
       }.bind(this));
     },
 
     loadModules: function(threadID) {
       for (var key in this.modules) {
-        this.rooms[threadID].modules[key] = this.modules[key](api);
+        this.rooms[threadID].modules[key] = this.modules[key].create(api, this.modules[key].args);
       }
     },
 
